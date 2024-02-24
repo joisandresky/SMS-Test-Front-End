9bdf48a1556811708e0dbc41f6e5151188f8c733
Jois Andresky <andreskyjois8@gmail.com>
2024-02-24 16:46:00 +0700

    adjustment when user already put in query in url

--- src/components/Course/SearchBox.jsx
+++ src/components/Course/SearchBox.jsx
@@ -4,13 +4,33 @@ import styles from "./search.module.css"
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function SearchBox() {
+    const searchParams = useSearchParams();
+    
     const [name, setName] = useState("")
     const [level, setLevel] = useState("")
     const [country, setCountry] = useState("")
 
-    const searchParams = useSearchParams();
     const pathname = usePathname();
     const router = useRouter();
+
+    // Check if 'name' parameter is present in the searchParams and set it to the 'name' state
+    useEffect(() => {
+        const nameFromParams = searchParams.get('name');
+        if (nameFromParams) {
+            console.log("ada", nameFromParams)
+            setName(nameFromParams);
+        }
+
+        const levelFromParams = searchParams.get('level');
+        if(levelFromParams) {
+            setLevel(levelFromParams);
+        }
+
+        const countryFromParams = searchParams.get('country');
+        if(countryFromParams) {
+            setCountry(countryFromParams);
+        }
+    }, [searchParams]);
 
     useEffect(() => {
         const debounceTimeout = setTimeout(() => {
@@ -47,9 +67,9 @@ export default function SearchBox() {
 
     return (
         <div className={styles.search_container}>
-            <input className={styles.search_input} type="text" name="name" placeholder="Search University name or Course name" onChange={e => setName(e.target.value)} />
-            <input className={styles.search_input} type="text" name="level" placeholder="Search Level e.g. Diploma or Undergraduate" onChange={e => setLevel(e.target.value)} />
-            <input className={styles.search_input} type="text" name="country" placeholder="Search Country e.g. India, United States of America, etc." onChange={e => setCountry(e.target.value)} />
+            <input className={styles.search_input} type="text" name="name" placeholder="Search University name or Course name" onChange={e => setName(e.target.value)} value={name}/>
+            <input className={styles.search_input} type="text" name="level" placeholder="Search Level e.g. Diploma or Undergraduate" onChange={e => setLevel(e.target.value)} value={level}/>
+            <input className={styles.search_input} type="text" name="country" placeholder="Search Country e.g. India, United States of America, etc." onChange={e => setCountry(e.target.value)} value={country}/>
         </div>
     )
 }
